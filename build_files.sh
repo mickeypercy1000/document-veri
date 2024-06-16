3eecfa97506287dd8f35627ec71649cf2113598d
MICHAEL AMOO <michael.amoo@wingipay.com>
2024-06-16 21:12:32 +0000

    build file setup

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,8 @@ source venv/bin/activate
 
 # install all deps in the venv
 pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 # collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
