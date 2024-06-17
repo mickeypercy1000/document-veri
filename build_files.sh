17685186bd1b5fbcbada201d324f58992bbd2ac5
MICHAEL AMOO <michael.amoo@wingipay.com>
2024-06-17 09:08:39 +0000

    static files

--- build_files.sh
+++ build_files.sh
@@ -1,24 +1,17 @@
 # # create a virtual environment named 'venv' if it doesn't already exist
-# python3.9 -m venv venv
+python3.9 -m venv venv
 
 # # activate the virtual environment
-# pip3 install --upgrade pip
-# source venv/bin/activate
+pip install --upgrade pip
+source venv/bin/activate
+pip install -r requirements.txt
 
 # # install all deps in the venv
 # # pip install django
 # # pip install cmake==3.25
 # # pip install dlib
 # pip install -r requirements.txt
-# python3 manage.py makemigrations --noinput
-# python3 manage.py migrate --noinput
 
-# # collect static files using the Python interpreter from venv
-# python3 manage.py collectstatic --noinput
 
-# [optional] Start the application here 
-# python manage.py runserver
-
-# build_files.sh
-pip install -r requirements.txt
 python3.9 manage.py collectstatic
+python manage.py runserver
