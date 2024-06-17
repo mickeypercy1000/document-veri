5c7df0317ff426615df70de5ebf9463df7a7f4f9
MICHAEL AMOO <michael.amoo@wingipay.com>
2024-06-17 01:06:37 +0000

    some changes

--- build_files.sh
+++ build_files.sh
@@ -6,12 +6,13 @@ source venv/bin/activate
 
 # install all deps in the venv
 pip install django
+pip install cmake
 pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 # collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # [optional] Start the application here 
 # python manage.py runserver
